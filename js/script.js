de7317e338f85ef0e8c5438ec55a57b9c52e4cc5
Rodrigo Martins <rodrigomartins.s@outlook.com>
2024-04-08 21:31:00 -0300

    Dia 01 - Criação da nova tela Inicial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.input', {
+        strings: ["Desenvolvedor Full-Stack"],
+        typeSpeed: 80,
+        backSpeed: 55,
+        loop: true
+    });
+});
